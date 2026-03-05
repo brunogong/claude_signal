b374eca526f3d48d40249f8d797ff22c021f6f95
brunogong <brunogong@gmail.com>
2026-03-05 17:51:08 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,5 @@ from .technical_analysis import TechnicalAnalyzer
 from .fundamental_analysis import FundamentalAnalyzer
 from .sentiment_analysis import SentimentAnalyzer
 from .signal_generator import SignalGenerator
+from .telegram_notifier import TelegramNotifier
+from .lot_calculator import LotCalculator
