16ebcfa2283de1a97c155efe770da14e1c09035b
brunogong <brunogong@gmail.com>
2026-03-05 17:37:25 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
+from .data_providers import FreeDataProvider
 from .technical_analysis import TechnicalAnalyzer
 from .fundamental_analysis import FundamentalAnalyzer
 from .sentiment_analysis import SentimentAnalyzer
 from .signal_generator import SignalGenerator
-from .utils import *
